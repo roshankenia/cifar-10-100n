6e226b07eb31c3e7a781204c838f02f40cfd35d8
Roshan Kenia <keniaroshan@gmail.com>
2022-09-15 14:52:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,21 +18,27 @@ else:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type = float, default = 0.1)
-parser.add_argument('--noise_type', type = str, help='clean, aggre, worst, rand1, rand2, rand3, clean100, noisy100', default='clean')
-parser.add_argument('--noise_path', type = str, help='path of CIFAR-10_human.pt', default=None)
-parser.add_argument('--dataset', type = str, help = ' cifar10 or cifar100', default = 'cifar10')
+parser.add_argument('--lr', type=float, default=0.1)
+parser.add_argument('--noise_type', type=str,
+                    help='clean, aggre, worst, rand1, rand2, rand3, clean100, noisy100', default='clean')
+parser.add_argument('--noise_path', type=str,
+                    help='path of CIFAR-10_human.pt', default=None)
+parser.add_argument('--dataset', type=str,
+                    help=' cifar10 or cifar100', default='cifar10')
 parser.add_argument('--n_epoch', type=int, default=100)
 parser.add_argument('--seed', type=int, default=0)
 parser.add_argument('--print_freq', type=int, default=50)
-parser.add_argument('--num_workers', type=int, default=4, help='how many subprocesses to use for data loading')
+parser.add_argument('--num_workers', type=int, default=4,
+                    help='how many subprocesses to use for data loading')
 parser.add_argument('--is_human', action='store_true', default=False)
 
 # Adjust learning rate and for SGD Optimizer
-def adjust_learning_rate(optimizer, epoch,alpha_plan):
+
+
+def adjust_learning_rate(optimizer, epoch, alpha_plan):
     for param_group in optimizer.param_groups:
-        param_group['lr']=alpha_plan[epoch]
-        
+        param_group['lr'] = alpha_plan[epoch]
+
 
 def accuracy(logit, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
@@ -51,38 +57,41 @@ def accuracy(logit, target, topk=(1,)):
     return res
 
 # Train the Model
+
+
 def train(epoch, train_loader, model, optimizer):
-    train_total=0
-    train_correct=0
+    train_total = 0
+    train_correct = 0
 
     for i, (images, labels, indexes) in enumerate(train_loader):
-        ind=indexes.cpu().numpy().transpose()
+        ind = indexes.cpu().numpy().transpose()
         batch_size = len(ind)
-       
+
         images = Variable(images).cuda()
         labels = Variable(labels).cuda()
-       
+
         # Forward + Backward + Optimize
         logits = model(images)
 
         prec, _ = accuracy(logits, labels, topk=(1, 5))
         # prec = 0.0
-        train_total+=1
-        train_correct+=prec
-        loss = F.cross_entropy(logits, labels, reduce = True)
+        train_total += 1
+        train_correct += prec
+        loss = F.cross_entropy(logits, labels, reduce=True)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if (i+1) % args.print_freq == 0:
-            print ('Epoch [%d/%d], Iter [%d/%d] Training Accuracy: %.4F, Loss: %.4f'
-                  %(epoch+1, args.n_epoch, i+1, len(train_dataset)//batch_size, prec, loss.data))
+            print('Epoch [%d/%d], Iter [%d/%d] Training Accuracy: %.4F, Loss: %.4f'
+                  % (epoch+1, args.n_epoch, i+1, len(train_dataset)//batch_size, prec, loss.data))
 
-
-    train_acc=float(train_correct)/float(train_total)
+    train_acc = float(train_correct)/float(train_total)
     return train_acc
 # test
 # Evaluate the Model
+
+
 def evaluate(test_loader, model):
     model.eval()    # Change model to 'eval' mode.
     # print('previous_best', best_acc_)
@@ -100,7 +109,6 @@ def evaluate(test_loader, model):
     return acc
 
 
-
 #####################################main code ################################################
 args = parser.parse_args()
 # Seed
@@ -110,7 +118,8 @@ torch.cuda.manual_seed(args.seed)
 # Hyper Parameters
 batch_size = 128
 learning_rate = args.lr
-noise_type_map = {'clean':'clean_label', 'worst': 'worse_label', 'aggre': 'aggre_label', 'rand1': 'random_label1', 'rand2': 'random_label2', 'rand3': 'random_label3', 'clean100': 'clean_label', 'noisy100': 'noisy_label'}
+noise_type_map = {'clean': 'clean_label', 'worst': 'worse_label', 'aggre': 'aggre_label', 'rand1': 'random_label1',
+                  'rand2': 'random_label2', 'rand3': 'random_label3', 'clean100': 'clean_label', 'noisy100': 'noisy_label'}
 args.noise_type = noise_type_map[args.noise_type]
 # load dataset
 if args.noise_path is None:
@@ -118,43 +127,54 @@ if args.noise_path is None:
         args.noise_path = './data/CIFAR-10_human.pt'
     elif args.dataset == 'cifar100':
         args.noise_path = './data/CIFAR-100_human.pt'
-    else: 
+    else:
         raise NameError(f'Undefined dataset {args.dataset}')
 
 
-train_dataset,test_dataset,num_classes,num_training_samples = input_dataset(args.dataset,args.noise_type, args.noise_path, args.is_human)
+train_dataset, test_dataset, num_classes, num_training_samples = input_dataset(
+    args.dataset, args.noise_type, args.noise_path, args.is_human)
+
+print("aggre:", num_training_samples)
+
+clean_train_dataset, clean_test_dataset, clean_num_classes, clean_num_training_samples = input_dataset(
+    args.dataset, 'clean_label', args.noise_path, args.is_human)
+
+print("clean:", clean_num_training_samples)
+
+exit()
+    
 noise_prior = train_dataset.noise_prior
 noise_or_not = train_dataset.noise_or_not
-print('train_labels:', len(train_dataset.train_labels), train_dataset.train_labels[:10])
+print('train_labels:', len(train_dataset.train_labels),
+      train_dataset.train_labels[:10])
 # load model
 print('building model...')
 model = ResNet34(num_classes)
 print('building model done')
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=0.0005, momentum=0.9)
+optimizer = torch.optim.SGD(
+    model.parameters(), lr=learning_rate, weight_decay=0.0005, momentum=0.9)
 
-train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
-                                   batch_size = 128,
-                                   num_workers=args.num_workers,
-                                   shuffle=True)
-
-
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=128,
+                                           num_workers=args.num_workers,
+                                           shuffle=True)
 
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                  batch_size = 64,
-                                  num_workers=args.num_workers,
-                                  shuffle=False)
+                                          batch_size=64,
+                                          num_workers=args.num_workers,
+                                          shuffle=False)
 alpha_plan = [0.1] * 60 + [0.01] * 40
 model.cuda()
 
 
-epoch=0
+epoch = 0
 train_acc = 0
 
 # training
 noise_prior_cur = noise_prior
 for epoch in range(args.n_epoch):
-# train models
+    # train models
     print(f'epoch {epoch}')
     adjust_learning_rate(optimizer, epoch, alpha_plan)
     model.train()
