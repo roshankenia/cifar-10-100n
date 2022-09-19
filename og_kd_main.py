d3c26360bc49ff6d289f0bb37b916dabcb294a15
Roshan Kenia <keniaroshan@gmail.com>
2022-09-19 15:48:59 -0400

    Update og_kd_main.py

--- og_kd_main.py
+++ og_kd_main.py
@@ -241,13 +241,13 @@ if args.noise_path is None:
 train_dataset, test_dataset, num_classes, num_training_samples = input_dataset(
     args.dataset, args.noise_type, args.noise_path, args.is_human)
 
-print("aggre:", num_training_samples.train_labels)
+print("aggre:", num_training_samples)
 # print(train_dataset[:500])
 
 clean_train_dataset, clean_test_dataset, clean_num_classes, clean_num_training_samples = input_dataset(
     args.dataset, 'clean_label', args.noise_path, args.is_human)
 
-print("clean:", clean_train_dataset)
+print("clean:", clean_train_dataset.train_labels)
 exit()
 noise_prior = train_dataset.noise_prior
 noise_or_not = train_dataset.noise_or_not
