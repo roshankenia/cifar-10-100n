4d61d0c5b631d27ad8494c602b1b1fa3b1778456
Roshan Kenia <keniaroshan@gmail.com>
2022-09-25 19:59:47 -0400

    Update mixup_main.py

--- mixup_main.py
+++ mixup_main.py
@@ -7,7 +7,7 @@ from data.datasets import input_dataset
 from models import *
 import argparse
 import sys
-import np
+import numpy as np
 # ensure we are running on the correct gpu
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "6"  # (xxxx is your specific GPU ID)
