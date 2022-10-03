de1f786899cca0cc3852af479eba8727c66bec54
Roshan Kenia <keniaroshan@gmail.com>
2022-10-03 16:20:13 -0400

    Update mixup_main.py

--- mixup_main.py
+++ mixup_main.py
@@ -121,6 +121,9 @@ def smart_mixup(x, y, alpha=1.0, use_cuda=True, num_classes=10):
         index_classes[y[index]].append(index)
         index_counts[y[index]] += 1
     index_total = index_counts.copy()
+    print('y:',y)
+    print('classes:',index_classes)
+    
     # now create new list of indexes to mix on
     indices = []
     for index in rand_index:
