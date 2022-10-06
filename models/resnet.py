337578cbb58d8c3b77a113933b13458c77658f69
Roshan Kenia <keniaroshan@gmail.com>
2022-10-05 21:43:57 -0400

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -206,7 +206,7 @@ class VecResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        # out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
