5e76a260fa2b4456fc63385bc5b1a8c06631d599
Roshan Kenia <keniaroshan@gmail.com>
2022-10-06 16:31:12 -0400

    Update datasets.py

--- data/datasets.py
+++ data/datasets.py
@@ -14,7 +14,6 @@ else:
     print('GPU is being properly used')
 
 vec_train_cifar10_transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
